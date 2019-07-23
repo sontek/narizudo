b6c592448807337c09fb87dcf1ba56114e4ca76e
John Anderson <sontek@gmail.com>
2019-07-23 17:17:41 -0400

    Initial implementation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Theme from './theme';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Theme />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,31 @@
+import { Link, Element } from "react-scroll";
+import { slide as Menu } from "react-burger-menu";
+
+import React from "react";
+import MediaQuery from "react-responsive";
+import Slider from "react-slick";
+import cx from "classnames";
+import gus from "./css/gus.module.scss";
+import styles from "./css/app.module.scss";
+import logo from "./img/narizudo.png";
+
+import { hidden } from "ansi-colors";
+
+
+class Theme extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={cx(styles.theme1, styles.theme)}>
+        <div className={styles.section1} id='section1'>
+          <img src={logo} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Theme;
