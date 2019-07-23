adc65f706c49bc6156d6add38dfdd5f34a2d2bdb
John Anderson <sontek@gmail.com>
2019-07-23 17:18:22 -0400

    Updates

---
+++ precache-manifest.d99a8b1bb685b1c34425f5a8e725003e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a51e295ff44b30b481d1",
+    "url": "/narizudo/static/css/main.ee2c2b76.chunk.css"
+  },
+  {
+    "revision": "a51e295ff44b30b481d1",
+    "url": "/narizudo/static/js/main.a51e295f.chunk.js"
+  },
+  {
+    "revision": "cc57e8717832d50374af",
+    "url": "/narizudo/static/js/1.cc57e871.chunk.js"
+  },
+  {
+    "revision": "a5e1370a54f8ef692e5e",
+    "url": "/narizudo/static/js/runtime~main.a5e1370a.js"
+  },
+  {
+    "revision": "2c63c4927eb2c2f14b5aa976691006cd",
+    "url": "/narizudo/static/media/MMCOFFICE-BOLD.2c63c492.ttf"
+  },
+  {
+    "revision": "22c0c6f62029a7d6da2f99988704718e",
+    "url": "/narizudo/static/media/MMCOFFICE-BOLD.22c0c6f6.eot"
+  },
+  {
+    "revision": "f0dce1ab57233d3ff7329643229366d2",
+    "url": "/narizudo/static/media/MMCOFFICE-REGULAR.f0dce1ab.eot"
+  },
+  {
+    "revision": "dd2c8a0d762ea7ac040d56d9caa82921",
+    "url": "/narizudo/static/media/MMCOFFICE-REGULAR.dd2c8a0d.ttf"
+  },
+  {
+    "revision": "6019097d78fac78b0e2c45acd62da359",
+    "url": "/narizudo/static/media/narizudo.6019097d.png"
+  },
+  {
+    "revision": "d2d2440d531d3a7e9dfd588c21ee8159",
+    "url": "/narizudo/index.html"
+  }
+];
